971e829eb70d7bea6980379b99f83ce75f8cdde0
Maruf Tuhin <maruf.2hin@gmail.com>
2018-08-15 19:08:46 +0600

    Use ObservedGeneration in Status to keep track of last generation observed (#62)
    xref: https://github.com/kubedb/project/issues/224

--- vendor/github.com/kubedb/apimachinery/apis/kubedb/v1alpha1/redis_helpers.go
+++ vendor/github.com/kubedb/apimachinery/apis/kubedb/v1alpha1/redis_helpers.go
@@ -2,8 +2,12 @@ package v1alpha1
 
 import (
 	"fmt"
+	"reflect"
 
+	"github.com/appscode/go/log"
 	crdutils "github.com/appscode/kutil/apiextensions/v1beta1"
+	meta_util "github.com/appscode/kutil/meta"
+	"github.com/golang/glog"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	mona "kmodules.xyz/monitoring-agent-api/api/v1"
 )
@@ -85,6 +89,7 @@ func (r Redis) CustomResourceDefinition() *apiextensions.CustomResourceDefinitio
 		Singular:      ResourceSingularRedis,
 		Kind:          ResourceKindRedis,
 		ShortNames:    []string{ResourceCodeRedis},
+		Categories:    []string{"datastore", "kubedb", "appscode"},
 		ResourceScope: string(apiextensions.NamespaceScoped),
 		Versions: []apiextensions.CustomResourceDefinitionVersion{
 			{
@@ -118,4 +123,67 @@ func (r Redis) CustomResourceDefinition() *apiextensions.CustomResourceDefinitio
 			},
 		},
 	}, setNameSchema)
+}
+
+func (r *Redis) Migrate() {
+	if r == nil {
+		return
+	}
+	r.Spec.Migrate()
+}
+
+func (r *RedisSpec) Migrate() {
+	if r == nil {
+		return
+	}
+	if len(r.NodeSelector) > 0 {
+		r.PodTemplate.Spec.NodeSelector = r.NodeSelector
+		r.NodeSelector = nil
+	}
+	if r.Resources != nil {
+		r.PodTemplate.Spec.Resources = *r.Resources
+		r.Resources = nil
+	}
+	if r.Affinity != nil {
+		r.PodTemplate.Spec.Affinity = r.Affinity
+		r.Affinity = nil
+	}
+	if len(r.SchedulerName) > 0 {
+		r.PodTemplate.Spec.SchedulerName = r.SchedulerName
+		r.SchedulerName = ""
+	}
+	if len(r.Tolerations) > 0 {
+		r.PodTemplate.Spec.Tolerations = r.Tolerations
+		r.Tolerations = nil
+	}
+	if len(r.ImagePullSecrets) > 0 {
+		r.PodTemplate.Spec.ImagePullSecrets = r.ImagePullSecrets
+		r.ImagePullSecrets = nil
+	}
+}
+
+func (r *Redis) Equal(other *Redis) bool {
+	if EnableStatusSubresource {
+		// At this moment, metadata.Generation is incremented only by `spec`.
+		// issue tracked: https://github.com/kubernetes/kubernetes/issues/67428
+		// So look for changes in metadata.labels as well.
+		if r.Generation <= r.Status.ObservedGeneration && reflect.DeepEqual(other.Labels, r.Labels) {
+			return true
+		}
+		if glog.V(log.LevelDebug) {
+			diff := meta_util.Diff(other, r)
+			glog.InfoDepth(1, "meta.Generation [%d] is higher than status.observedGeneration [%d] in Redis %s/%s with Diff: %s",
+				r.Generation, r.Status.ObservedGeneration, r.Namespace, r.Name, diff)
+		}
+		return false
+	}
+
+	if !meta_util.Equal(other.Spec, r.Spec) || !reflect.DeepEqual(other.Labels, r.Labels) {
+		if glog.V(log.LevelDebug) {
+			diff := meta_util.Diff(other, r)
+			glog.InfoDepth(1, "Redis %s/%s has changed. Diff: %s", r.Namespace, r.Name, diff)
+		}
+		return false
+	}
+	return true
 }
